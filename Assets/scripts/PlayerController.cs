710a2904b45136effbfe4a06b1f2f2178c3e14d8
obinyx <manel070807@gmail.com>
2025-12-10 13:55:10 +0100

    movimientos

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,18 +8,38 @@ public class PlayerController : MonoBehaviour
     public Vector3 startPosition;
 
     public float movementSpeed = 5f;
+    public float jumpForce = 10;
 
-    public int direction = 1;
+
+    private InputAction moveAction;
+    public Vector2 moveDirection;
+
+    public Rigidbody2D rBody2D;
+
+    void Awake()
+    {
+        rBody2D = GetComponent<Rigidbody2D>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = startPosition;
+
+        moveAction = InputSystem.actions["Move"];
+
+        rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + direction * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x + direction * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        moveDirection = moveAction.ReadValue<Vector2>();
+
+        transform.Translate(new Vector3(moveDirection.x * movementSpeed * Time.deltaTime, 0, 0));
+
+        //transform.position = new Vector3(transform.position.x + moveDirection.x * movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
     }
+    
 }
